cff942961acfd92bbf6063478d72b16e5620e359
Harsh Rohila <harsh.rohila@mrisoftware.com>
2021-07-04 19:48:34 +0530

    added font awesome 5

---
+++ app/components/features-list/component.ts
@@ -0,0 +1,13 @@
+import Component from '@glimmer/component';
+
+interface FeaturesListArgs {}
+
+export default class FeaturesList extends Component<FeaturesListArgs> {
+	features = [
+		'TypeScript',
+		'Using Pods folder structure',
+		'Removed IE 11 support',
+		'Bootstrap 5',
+		'Fontawesome 5',
+	];
+}
